3d81fd315901bbb46302a73a0c8a4595d6fc95c0
drive <drive@proton.ch>
2025-02-10 12:06:20 +0000

    Seed integration tests for CLIs

---
+++ js/cli/src/proton-drive.ts
@@ -0,0 +1,76 @@
+import { parseArgs } from "util";
+
+import { init } from "./init";
+import { commands } from "./commands";
+
+const { account, sdk, fileSystem } = await init();
+
+const commandName = Bun.argv[2];
+
+function printCommandUsage(name: string, command: typeof commands[keyof typeof commands]) {
+    const args = (command.args || []).map(arg => `<${arg}>`).join(' ');
+    const options = Object.entries(command.options || {}).map(([name, { type }]) => `--${name} <${type}>`).join(' ');
+    console.log(`  ${name} ${args} ${options}`);
+}
+
+const command = commands[commandName];
+if (!command) {
+    console.log(`Command not found: ${commandName}`);
+
+    console.log("Usage:");
+    Object.entries(commands).map(([name, command]) => {
+        printCommandUsage(name, command);
+    });
+
+    process.exit(1);
+}
+
+const { values, positionals } = parseArgs({
+    args: Bun.argv,
+    options: command.options || {},
+    strict: true,
+    allowPositionals: true,
+});
+
+const args = positionals.slice(3);
+
+if (command.args) {
+    if (args.length !== command.args.length) {
+        console.log(`Expected ${command.args.length} arguments, got ${args.length}`);
+
+        console.log("Usage:");
+        printCommandUsage(commandName, command);
+
+        process.exit(1);
+    }
+}
+
+Object.keys(command.options || {}).forEach((key) => {
+    if (values[key] === undefined) {
+        console.log(`Missing required option: ${key}`);
+
+        console.log("Usage:");
+        printCommandUsage(commandName, command);
+
+        process.exit(1);
+    }
+});
+
+if (!command.isAuthAction) {
+    if (!account.session) {
+        console.log("You need to login first");
+        process.exit(1);
+    }
+
+    await account.loadPrimaryKeys();
+}
+
+await command.action({
+    account,
+    sdk,
+    fileSystem,
+    args,
+    options: values,
+});
+
+process.exit()
