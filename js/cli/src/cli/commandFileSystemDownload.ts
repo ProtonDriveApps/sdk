2545d9cf51e2c0d7edb2998f04f350807b737fa3
drive <drive@proton.ch>
2025-05-29 11:58:30 +0200

    fix CLI download command to follow documentation

--- js/cli/src/cli/commandFileSystemDownload.ts
+++ js/cli/src/cli/commandFileSystemDownload.ts
@@ -1,3 +1,6 @@
+import path from 'node:path';
+import { ParseArgsConfig } from "util";
+
 import { Command, ActionArgs } from './interface';
 import { getName } from './node';
 
@@ -6,12 +9,21 @@ export class CommandFileSystemDownload implements Command {
     name = 'download';
     // FIXME: support download of multiple files
     args = ['path', 'localPath'];
+    options: ParseArgsConfig['options'] = {
+        name: {
+            type: 'string',
+            short: 'n',
+            default: '',
+        },
+    };
 
-    async action({ sdk, paths, args: [ pathString, localPath ] }: ActionArgs) {
+    async action({ sdk, paths, args: [ pathString, localParentPath ], options: { name } }: ActionArgs) {
         const nodePath = paths.getPath(pathString);
         const node = await nodePath.getNode();
         const downloader = await sdk.getFileDownloader(node);
+
         const claimedSize = downloader.getClaimedSizeInBytes();
+        const localPath = path.join(localParentPath, name || getName(node));
         console.log(`Downloading ${getName(node)} (${claimedSize || 'N/A'} bytes) to ${localPath}`);
 
         const file = Bun.file(localPath);
