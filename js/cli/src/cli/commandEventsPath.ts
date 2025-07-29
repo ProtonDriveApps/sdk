83408e7944838b004888a502f8ff47c2d7c88854
drive <drive@proton.ch>
2025-07-29 13:22:28 +0000

    Refactor event manager:

---
+++ js/cli/src/cli/commandEventsPath.ts
@@ -0,0 +1,32 @@
+import { Command, ActionArgs } from "./interface";
+import { runForever, eventsCallback } from "./events";
+import { DriveEvent, DriveEventType } from "../../../sdk/src";
+
+export class CommandEventsPath implements Command {
+    group = "events";
+    name = "path";
+    args = ["path"];
+
+    async action({ sdk, paths, args: [pathString], options: { json } }: ActionArgs) {
+        const nodePath = paths.getPath(pathString);
+        const maybeNode = await nodePath.getNode();
+        if (!maybeNode.ok) {
+            throw Error("Folder not found");
+        }
+        const node = maybeNode.value;
+
+        const filter = (event: DriveEvent) => {
+            if (![DriveEventType.NodeCreated, DriveEventType.NodeUpdated, DriveEventType.TreeRefresh].includes(event.type)) {
+                return false;
+            }
+            if ('parentNodeUid' in event) {
+                // If node is deleted, parent uid may no longer exist, so better to return all deletions
+                return event.parentNodeUid === node.uid || event.parentNodeUid === null;
+            }
+            return false;
+        }
+
+        await sdk.subscribeToTreeEvents(node.treeEventScopeId, async (event: DriveEvent) => eventsCallback(json, filter, event));
+        await runForever();
+    }
+}
--- js/cli/src/cli/events.ts
+++ js/cli/src/cli/events.ts
@@ -1,12 +1,37 @@
+import { DriveEvent, DriveEventType } from "../../../sdk/src";
+
 export async function runForever() {
     return new Promise(() => {});
-} 
+}
 
-export function eventsCallback(json: boolean, event: any) {
+function mapEventToIcon(eventType: DriveEventType): string {
+    switch(eventType) {
+        case DriveEventType.NodeUpdated:
+            return '♻️';
+        case DriveEventType.NodeCreated:
+            return '🐤';
+        case DriveEventType.NodeDeleted:
+            return '❌';
+        case DriveEventType.TreeRefresh:
+            return '🐤';
+        case DriveEventType.SharedWithMeUpdated:
+            return '👪';
+        case DriveEventType.TreeRemove:
+            return '🪓';
+        case DriveEventType.FastForward:
+            return '⏩';
+    }
+}
+
+export function eventsCallback(json: boolean, filter: (event: DriveEvent) => boolean, event: DriveEvent) {
+
+    if (!filter(event)) {
+        return;
+    }
     if (json) {
         console.log(JSON.stringify(event));
     } else {
-        const icon = event.type === 'update' ? '♻️' : event.type === 'remove' ? '❌' : '';
+        const icon = mapEventToIcon(event.type)
         console.log(icon, event);
     }
 }
