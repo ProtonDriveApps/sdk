b9cea18045bc6834a18479b3828a23e8c44e1591
drive <drive@proton.ch>
2025-10-31 08:10:43 +0100

    Fix deserialization error on download

--- cs/sdk/src/Proton.Drive.Sdk/Api/Files/BlockDto.cs
+++ cs/sdk/src/Proton.Drive.Sdk/Api/Files/BlockDto.cs
@@ -7,6 +7,7 @@ internal sealed class BlockDto
 {
     public required int Index { get; init; }
 
+    [JsonPropertyName("Hash")]
     [JsonConverter(typeof(ForgivingBytesToHexJsonConverter))]
     public required ReadOnlyMemory<byte> HashDigest { get; init; }
 
