b8604cd143ae5c580069723e247a0a7a09b0508e
drive <drive@proton.ch>
2025-11-21 14:36:38 +0000

    Add pause and resume API

--- kt/sdk/src/main/kotlin/me/proton/drive/sdk/UploadController.kt
+++ kt/sdk/src/main/kotlin/me/proton/drive/sdk/UploadController.kt
@@ -19,17 +19,24 @@
 package me.proton.drive.sdk
 
 import me.proton.drive.sdk.internal.JniUploadController
+import java.io.InputStream
 
 class UploadController internal constructor(
     uploader: Uploader,
     internal val handle: Long,
     private val bridge: JniUploadController,
+    private val inputStream: InputStream,
     override val cancellationTokenSource: CancellationTokenSource,
 ) : SdkNode(uploader), AutoCloseable, Cancellable {
 
     suspend fun awaitCompletion() = bridge.awaitCompletion(handle)
 
+    suspend fun resume() = bridge.resume(handle)
+
+    suspend fun pause() = bridge.pause(handle)
+
     override fun close() {
+        inputStream.close()
         bridge.free(handle)
         super.close()
     }
