cff9b08170720522c672c1b0c9f9bcd476f7fe75
drive <drive@proton.ch>
2025-12-18 17:02:22 +0000

    Fix download failures due to missing keys for manifest check

--- kt/sdk/src/main/kotlin/me/proton/drive/sdk/CorePublicAddressResolver.kt
+++ kt/sdk/src/main/kotlin/me/proton/drive/sdk/CorePublicAddressResolver.kt
@@ -9,12 +9,15 @@ class CorePublicAddressResolver(
     private val publicAddressRepository: PublicAddressRepository,
 ) : PublicAddressResolver {
 
-    override suspend fun getAddressPublicKeys(emailAddress: String): List<ByteArray> =
-        publicAddressRepository.getPublicAddressInfo(
+    override suspend fun getAddressPublicKeys(emailAddress: String): List<ByteArray> {
+        val publicAddressInfo = publicAddressRepository.getPublicAddressInfo(
             sessionUserId = userId,
             email = emailAddress
-        ).address.keys.publicKeyRing().keys.map { publicKey ->
+        )
+        val publicAddressKeys = publicAddressInfo.address.keys + publicAddressInfo.unverified?.keys.orEmpty()
+        return publicAddressKeys.publicKeyRing().keys.map { publicKey ->
             publicKey.key.toByteArray()
         }
+    }
 
 }
