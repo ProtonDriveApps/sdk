5e986b93ceee72d9d4d8b78dd831d787add3edad
drive <drive@proton.ch>
2025-12-19 13:14:12 +0000

    Expose download integrity errors and download status

--- cs/sdk/src/Proton.Drive.Sdk/Nodes/Download/DownloadController.cs
+++ cs/sdk/src/Proton.Drive.Sdk/Nodes/Download/DownloadController.cs
@@ -1,11 +1,38 @@
 ﻿namespace Proton.Drive.Sdk.Nodes.Download;
 
-public sealed class DownloadController(Task downloadTask)
+public sealed class DownloadController
 {
+    private readonly Task _downloadTask;
+    private bool _isDownloadCompleteWithVerificationIssue;
+
+    internal DownloadController(Task downloadTask)
+    {
+        _downloadTask = downloadTask;
+        Completion = WrapDownloadTaskAsync();
+    }
+
     // FIXME
     public bool IsPaused { get; }
 
-    public Task Completion { get; } = downloadTask;
+    public bool GetIsDownloadCompleteWithVerificationIssue()
+    {
+        return _isDownloadCompleteWithVerificationIssue;
+    }
+
+    public Task Completion { get; private set; }
+
+    private async Task WrapDownloadTaskAsync()
+    {
+        try
+        {
+            await _downloadTask.ConfigureAwait(false);
+        }
+        catch (CompletedDownloadManifestVerificationException error)
+        {
+            _isDownloadCompleteWithVerificationIssue = true;
+            throw new DataIntegrityException(error.Message);
+        }
+    }
 
     public void Pause()
     {
