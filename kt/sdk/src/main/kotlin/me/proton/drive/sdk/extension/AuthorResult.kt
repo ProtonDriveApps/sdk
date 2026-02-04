815cdf800cf0e4883c8548d1d3bc7bc37552d907
drive <drive@proton.ch>
2026-02-04 07:24:56 +0000

    Make author and signature verification error mutually exclusive in interop

--- kt/sdk/src/main/kotlin/me/proton/drive/sdk/extension/AuthorResult.kt
+++ kt/sdk/src/main/kotlin/me/proton/drive/sdk/extension/AuthorResult.kt
@@ -4,13 +4,19 @@ import me.proton.drive.sdk.SignatureVerificationException
 import me.proton.drive.sdk.entity.Author
 import proton.drive.sdk.ProtonDriveSdk
 
-fun ProtonDriveSdk.AuthorResult.toEntity(): Result<Author> = if (signatureVerificationError.isEmpty()) {
-    Result.success(author.toEntity())
-} else {
-    Result.failure(
-        SignatureVerificationException(
-            claimedAuthor = author.toEntity(),
-            message = signatureVerificationError
-        )
-    )
-}
+fun ProtonDriveSdk.AuthorResult.toEntity(): Result<Author> =
+    when (resultCase) {
+        ProtonDriveSdk.AuthorResult.ResultCase.VALUE ->
+            Result.success(value.toEntity())
+
+        ProtonDriveSdk.AuthorResult.ResultCase.ERROR ->
+            Result.failure(
+                SignatureVerificationException(
+                    claimedAuthor = error.claimedAuthor.toEntity(),
+                    message = error.message
+                )
+            )
+
+        ProtonDriveSdk.AuthorResult.ResultCase.RESULT_NOT_SET, null ->
+            error("Invalid AuthorResult: result not set")
+    }
