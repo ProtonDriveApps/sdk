283377b75a4038f6601acb786e7894140b261856
drive <drive@proton.ch>
2026-03-19 08:49:06 +0100

    Improve error details for drive errors

--- kt/sdk/src/main/kotlin/me/proton/drive/sdk/ProtonDriveException.kt
+++ kt/sdk/src/main/kotlin/me/proton/drive/sdk/ProtonDriveException.kt
@@ -5,7 +5,12 @@ import me.proton.drive.sdk.entity.Author
 open class ProtonDriveException(
     override val message: String? = null,
     override val cause: Throwable? = null,
-) : Throwable(message, cause)
+) : Throwable(
+    /* message = */ message,
+    /* cause = */ cause,
+    /* enableSuppression = */ true,
+    /* writableStackTrace = */ false,
+)
 
 class SignatureVerificationException(
     val claimedAuthor: Author,
