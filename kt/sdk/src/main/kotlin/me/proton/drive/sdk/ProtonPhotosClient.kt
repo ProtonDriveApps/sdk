df4c725a32bd0f56ea0f12ad62903dd9fbd76921
drive <drive@proton.ch>
2026-03-23 15:15:29 +0100

    Add trash management to Photos

--- kt/sdk/src/main/kotlin/me/proton/drive/sdk/ProtonPhotosClient.kt
+++ kt/sdk/src/main/kotlin/me/proton/drive/sdk/ProtonPhotosClient.kt
@@ -1,12 +1,12 @@
 package me.proton.drive.sdk
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import me.proton.drive.sdk.LoggerProvider.Level.DEBUG
 import me.proton.drive.sdk.LoggerProvider.Level.INFO
 import me.proton.drive.sdk.entity.FileThumbnail
 import me.proton.drive.sdk.entity.NodeResult
+import me.proton.drive.sdk.entity.NodeResultPair
 import me.proton.drive.sdk.entity.NodeUid
 import me.proton.drive.sdk.entity.PhotosTimelineItem
 import me.proton.drive.sdk.entity.ThumbnailType
@@ -17,9 +17,14 @@ import me.proton.drive.sdk.internal.ProtonDriveSdkNativeClient
 import me.proton.drive.sdk.internal.cancellationCoroutineScope
 import me.proton.drive.sdk.internal.factory
 import me.proton.drive.sdk.internal.toLogId
+import proton.drive.sdk.drivePhotosClientDeleteNodesRequest
+import proton.drive.sdk.drivePhotosClientEmptyTrashRequest
 import proton.drive.sdk.drivePhotosClientEnumerateThumbnailsRequest
 import proton.drive.sdk.drivePhotosClientEnumerateTimelineRequest
+import proton.drive.sdk.drivePhotosClientEnumerateTrashRequest
 import proton.drive.sdk.drivePhotosClientGetNodeRequest
+import proton.drive.sdk.drivePhotosClientRestoreNodesRequest
+import proton.drive.sdk.drivePhotosClientTrashNodesRequest
 
 class ProtonPhotosClient internal constructor(
     internal val handle: Long,
@@ -68,6 +73,72 @@ class ProtonPhotosClient internal constructor(
                 cancellationTokenSourceHandle = source.handle
             }
         )?.toEntity()
+    }
+
+    suspend fun trashNodes(
+        nodeUids: List<NodeUid>,
+    ): List<NodeResultPair> = cancellationCoroutineScope { source ->
+        log(INFO, "trashNodes(${nodeUids.size} nodes)")
+        bridge.trashNodes(
+            drivePhotosClientTrashNodesRequest {
+                this.nodeUids += nodeUids.map { it.value }
+                clientHandle = handle
+                cancellationTokenSourceHandle = source.handle
+            }
+        ).toEntity()
+    }
+
+    suspend fun deleteNodes(
+        nodeUids: List<NodeUid>,
+    ): List<NodeResultPair> = cancellationCoroutineScope { source ->
+        log(INFO, "deleteNodes(${nodeUids.size} nodes)")
+        bridge.deleteNodes(
+            drivePhotosClientDeleteNodesRequest {
+                this.nodeUids += nodeUids.map { it.value }
+                clientHandle = handle
+                cancellationTokenSourceHandle = source.handle
+            }
+        ).toEntity()
+    }
+
+    suspend fun restoreNodes(
+        nodeUids: List<NodeUid>,
+    ): List<NodeResultPair> = cancellationCoroutineScope { source ->
+        log(INFO, "restoreNodes(${nodeUids.size} nodes)")
+        bridge.restoreNodes(
+            drivePhotosClientRestoreNodesRequest {
+                this.nodeUids += nodeUids.map { it.value }
+                clientHandle = handle
+                cancellationTokenSourceHandle = source.handle
+            }
+        ).toEntity()
+    }
+
+    fun enumerateTrash(): Flow<NodeResult> = channelFlow {
+        log(DEBUG, "enumerateTrash")
+        cancellationCoroutineScope { source ->
+            bridge.enumerateTrash(
+                coroutineScope = this@channelFlow,
+                drivePhotosClientEnumerateTrashRequest {
+                    clientHandle = handle
+                    cancellationTokenSourceHandle = source.handle
+                    iterateAction = ProtonDriveSdkNativeClient.getEnumeratePointer()
+                },
+                enumerate = { nodeResult ->
+                    send(nodeResult.toEntity())
+                }
+            )
+        }
+    }
+
+    suspend fun emptyTrash(): Unit = cancellationCoroutineScope { source ->
+        log(INFO, "emptyTrash")
+        bridge.emptyTrash(
+            drivePhotosClientEmptyTrashRequest {
+                clientHandle = handle
+                cancellationTokenSourceHandle = source.handle
+            }
+        )
     }
 
     override fun close() {
