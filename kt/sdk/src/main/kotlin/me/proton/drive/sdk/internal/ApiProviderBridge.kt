e3a756ac451b2f06acd1629f5cb6c4c1e7bda1ed
drive <drive@proton.ch>
2026-04-04 09:01:58 +0200

    Log network error and retries in kotlin

--- kt/sdk/src/main/kotlin/me/proton/drive/sdk/internal/ApiProviderBridge.kt
+++ kt/sdk/src/main/kotlin/me/proton/drive/sdk/internal/ApiProviderBridge.kt
@@ -29,11 +29,11 @@ internal class ApiProviderBridge(
     override suspend fun invoke(request: HttpRequest): HttpResponse {
         val httpStream = createHttpStream()
         val preparedRequest = request.prepare(httpStream)
-        val apiResult = RetryAfterDelay(isEnabled = preparedRequest.isRetryEnabled) {
+        val apiResult = RetryAfterDelay(isEnabled = preparedRequest.isRetryEnabled) { attempt ->
             apiProvider.get<HttpSdkApi>(userId).invoke(
                 forceNoRetryOnConnectionErrors = true
             ) {
-                execute(preparedRequest)
+                execute(preparedRequest, attempt)
             }
         }
         if (apiResult is ApiResult.Error) {
@@ -98,25 +98,41 @@ internal class ApiProviderBridge(
 
     private suspend fun HttpSdkApi.execute(
         request: PreparedRequest,
-    ): Response<ResponseBody> = with(request) {
-        logger("--> $method $url ($bodyMessage body)")
-        when (method.uppercase()) {
-            "GET" -> if (isDownloadBlock) {
-                getStreaming(url, headers)
-            } else {
-                get(url, headers)
-            }
+        attempt: Int,
+    ): Response<ResponseBody> = executeLogged(request, attempt) {
+        with(request) {
+            when (method.uppercase()) {
+                "GET" -> if (isDownloadBlock) {
+                    getStreaming(url, headers)
+                } else {
+                    get(url, headers)
+                }
 
-            "POST" -> post(url, headers, body)
-            "PUT" -> put(url, headers, body)
-            "DELETE" -> delete(url, headers, body)
-            else -> throw IllegalArgumentException("Unsupported method: $method")
-        }.also { response ->
-            val contentLength = response.body()?.contentLength()
-            val bodySize = if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
-            logger(
-                "<-- ${response.code()} ${response.message()} $url ($bodySize body)"
-            )
+                "POST" -> post(url, headers, body)
+                "PUT" -> put(url, headers, body)
+                "DELETE" -> delete(url, headers, body)
+                else -> throw IllegalArgumentException("Unsupported method: $method")
+            }
+        }
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    suspend fun HttpSdkApi.executeLogged(
+        request: PreparedRequest,
+        attempt: Int,
+        block: suspend HttpSdkApi.(PreparedRequest) -> Response<ResponseBody>,
+    ) = with(request) {
+        val attemptSuffix = if (attempt > 0) " [retry $attempt]" else ""
+        try {
+            logger("--> $method $url ($bodyMessage body)$attemptSuffix")
+            block(request).also { response ->
+                val contentLength = response.body()?.contentLength()
+                val bodySize = if (contentLength != -1L) "$contentLength-byte" else "unknown-length"
+                logger("<-- ${response.code()} ${response.message()} $url ($bodySize body)$attemptSuffix")
+            }
+        } catch (e: Exception) {
+            logger("<-- HTTP FAILED: $url ($e)$attemptSuffix")
+            throw e
         }
     }
 
