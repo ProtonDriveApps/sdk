1b7ee69c24aee061d6d1a62309d011f6e438f53b
drive <drive@proton.ch>
2026-04-22 05:58:02 +0000

    Add thumbnail error handling from API response

--- cs/sdk/src/Proton.Drive.Sdk/Api/Files/ThumbnailBlockListResponse.cs
+++ cs/sdk/src/Proton.Drive.Sdk/Api/Files/ThumbnailBlockListResponse.cs
@@ -7,4 +7,7 @@ internal sealed class ThumbnailBlockListResponse : ApiResponse
 {
     [JsonPropertyName("Thumbnails")]
     public required IReadOnlyList<ThumbnailBlock> Blocks { get; init; }
+
+    [JsonPropertyName("Errors")]
+    public required IReadOnlyList<ThumbnailBlockError> Errors { get; init; }
 }
