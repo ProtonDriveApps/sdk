a0ba9f1cf2e5c05eb4d10503e98cbffe8a5d6236
drive <drive@proton.ch>
2026-04-30 14:42:35 +0200

    Add error for verification error event

--- cs/sdk/src/Proton.Drive.Sdk/Telemetry/TelemetryRecorder.cs
+++ cs/sdk/src/Proton.Drive.Sdk/Telemetry/TelemetryRecorder.cs
@@ -33,33 +33,6 @@ internal static class TelemetryRecorder
     }
 
     /// <summary>
-    /// Attempts to record a decryption error event for a single field using a node UID.
-    /// </summary>
-    public static async Task TryRecordDecryptionErrorAsync(
-        ProtonDriveClient client,
-        NodeUid nodeUid,
-        EncryptedField field,
-        DateTime creationTime,
-        CancellationToken cancellationToken)
-    {
-        try
-        {
-            var @event = await TelemetryEventFactory.CreateDecryptionErrorEventAsync(
-                client,
-                nodeUid,
-                field,
-                creationTime,
-                cancellationToken).ConfigureAwait(false);
-
-            client.Telemetry.RecordMetric(@event);
-        }
-        catch
-        {
-            // Do nothing - telemetry failures should not break the main flow
-        }
-    }
-
-    /// <summary>
     /// Attempts to record a verification error event using a node UID.
     /// </summary>
     public static async Task TryRecordVerificationErrorAsync(
@@ -67,6 +40,7 @@ internal static class TelemetryRecorder
         NodeUid nodeUid,
         EncryptedField field,
         DateTime creationTime,
+        string? error,
         CancellationToken cancellationToken)
     {
         try
@@ -76,6 +50,7 @@ internal static class TelemetryRecorder
                 nodeUid,
                 field,
                 creationTime,
+                error,
                 cancellationToken).ConfigureAwait(false);
 
             client.Telemetry.RecordMetric(@event);
