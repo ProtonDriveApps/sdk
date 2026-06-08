1603bb4221db9e59ab89e0153ff771d9942bff54
drive <drive@proton.ch>
2026-06-08 14:04:12 +0000

    Remove unneeded hash digest field on block listing

--- cs/sdk/src/Proton.Drive.Sdk/Api/Files/BlockDto.cs
+++ cs/sdk/src/Proton.Drive.Sdk/Api/Files/BlockDto.cs
@@ -1,15 +1,10 @@
 ﻿using System.Text.Json.Serialization;
-using Proton.Sdk.Serialization;
 
 namespace Proton.Drive.Sdk.Api.Files;
 
 internal sealed class BlockDto
 {
     public required int Index { get; init; }
-
-    [JsonPropertyName("Hash")]
-    [JsonConverter(typeof(ForgivingBytesToHexJsonConverter))]
-    public required ReadOnlyMemory<byte> HashDigest { get; init; }
 
     [JsonPropertyName("BareURL")]
     public required string BareUrl { get; init; }
